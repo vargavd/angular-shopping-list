a1efa0e00bcbb564624f52fe985e6668cc21aae7
vargavd <vvd007@gmail.com>
2021-04-10 19:32:40 +0200

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'vdsl-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'vd-angular-shopping-list';
+}
